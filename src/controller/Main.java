d8727d6e76e8ee9c8c6f6998b8597ce1370aa124
shaik-md-musayyeb <smd52952@gmail.com>
2020-03-22 15:43:35 -0700

    modified

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,13 @@
+package controller;
+import java.io.IOException;
+import java.sql.SQLException;
 
+import utility.ConnectionManager;
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+		// TODO Auto-generated method stub
+		ConnectionManager.getConnection();
+	}
+
+}
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,1 +1,30 @@
+package utility;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionManager {
+	public static Properties loadPropertiesFile() throws IOException {
+		Properties p = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		p.load(in);
+		in.close();
+		return p;
+	}
+	public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
+		Connection conn = null;
+		Properties p = loadPropertiesFile();
+		String driver = p.getProperty("driver");
+		String url = p.getProperty("url");
+		Class.forName(driver);
+		conn = DriverManager.getConnection(url);
+//		if(conn != null)
+//			System.out.println("sucess");
+		return conn;
+	}
+}
 
